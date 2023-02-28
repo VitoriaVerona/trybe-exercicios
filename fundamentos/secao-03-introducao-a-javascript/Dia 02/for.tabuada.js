08a58a83298589c578eea0a25e68b785eec14c27
Vitória Verona <vitoriaverona2@gmail.com>
2023-02-28 14:34:36 -0400

    conteúdo for

---
+++ fundamentos/secao-03-introducao-a-javascript/Dia 02/for.tabuada.js
@@ -0,0 +1,7 @@
+/* Tabuada */
+let numero = 4;
+
+/* for (inicialização; condição de repetição; incremento/decremento) */
+for(let contador = 1; contador <= 10; contador += 1) {
+    console.log(numero*contador);
+}
