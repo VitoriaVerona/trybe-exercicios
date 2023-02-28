2a7ecbc5e270a939f1f66129b55640249d84a397
Vitória Verona <vitoriaverona2@gmail.com>
2023-02-28 14:16:15 -0400

    Para fixar array

---
+++ fundamentos/secao-03-introducao-a-javascript/Dia 02/array.dia.02.js
@@ -0,0 +1,8 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+let indexPortfolio = menu.indexOf('Portfólio');
+menu.push('Contato');
+
+console.log(indexPortfolio);
+console.log(menuServices);
+console.log(menu);
