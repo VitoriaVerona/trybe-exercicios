76b256ea109316f9356c202723d0f6db03740df3
Vitória Verona <vitoriaverona2@gmail.com>
2023-03-01 12:53:28 -0400

    Academia de lógica

---
+++ fundamentos/secao-03-introducao-a-javascript/Academia de lógica/exercicio.academia.de.logica.impar.js
@@ -0,0 +1,8 @@
+let impar = [];
+
+for (let index = 1; index <= 50; index += 1) {
+  if (index % 2 == 1) {
+    impar.push(index)
+  }
+}
+console.log(impar);
---
+++ fundamentos/secao-03-introducao-a-javascript/Academia de lógica/exercicio.academia.de.logica.index.of.js
@@ -0,0 +1,8 @@
+let arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let element = 3;
+
+if (arrayNumbers.indexOf(element) >= 0) {
+  console.log(arrayNumbers.indexOf(element));
+} else {
+  console.log('Elemento não encontrado no array');
+}
